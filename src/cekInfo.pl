2ee291e2aebcf04d2374f41ed81485147756bd15
Medcro <muhammaddhiyarafi@gmail.com>
2026-05-12 10:36:08 +0700

    feat: update cekInfo.pl (add base UI and logic)

--- src/cekInfo.pl
+++ src/cekInfo.pl
@@ -1,4 +1,20 @@
+tulisPemain(Urutan, JumlahPemain,, [NamaTerakhir]) :-
+    Urutan > JumlahPemain,
+    !.
+
+tulisPemain(Urutan, JumlahPemain, [Nama | SisaNama]) :-
+    write('Nama pemain'), write(Urutan), write(': '), write(Nama), nl,
+    player(Nama, DaftarKartu),
+    length(DaftarKartu, X),
+    write('Jumlah kartu : '), write(X), nl, nl,
+    NextUrutan is Urutan + 1,
+    tulisPemain(NextUrutan, JumlahPemain, SisaNama).
+
+
 cek_info :-
-    write('Kartu discard top: '),
-    write('Urutan pemain: '),
-    tulisListUrutan(UrutanList).
+    urutanPemain(ListUrutan),
+    jumlahPemain(JumlahPemain), 
+    write('Kartu discard top: '), write(kartu_angka(H)), nl, nl,
+    write('Urutan pemain: '), 
+    tulisListUrutan(ListUrutan), nl,
+    tulisPemain(1, JumlahPemain, ListUrutan).
