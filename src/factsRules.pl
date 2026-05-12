7c8bd45bd213ac7039ed6a605307f4b41debd55e
Gie <threegie.gendhis19@gmail.com>
2026-05-12 12:05:13 +0700

    fix: update factsRules.pl

--- src/factsRules.pl
+++ src/factsRules.pl
@@ -1,35 +1,72 @@
+/* FAKTA */
 
-/* FAKTA */ 
+kartu(merah, 0).
+kartu(kuning, 0).
+kartu(hijau, 0).
+kartu(biru, 0).
 
-pemain(X).
+kartu(merah, 1).
+kartu(kuning, 1).
+kartu(hijau, 1).
+kartu(biru, 1).
 
-/* RULES */ 
+kartu(merah, 2).
+kartu(kuning, 2).
+kartu(hijau, 2).
+kartu(biru, 2).
 
-read pemain(X).
+kartu(merah, 3).
+kartu(kuning, 3).
+kartu(hijau, 3).
+kartu(biru, 3).
 
-kartu(X, Y) :-  /* WARNA, JENIS */ 
-    kartu(merah, angka),
-    kartu(kuning, angka),
-    kartu(hijau, angka),
-    kartu(biru, angka),
+kartu(merah, 4).
+kartu(kuning, 4).
+kartu(hijau, 4).
+kartu(biru, 4).
 
-    kartu(merah, skip),
-    kartu(kuning, skip),
-    kartu(hijau, skip),
-    kartu(biru, skip),
+kartu(merah, 5).
+kartu(kuning, 5).
+kartu(hijau, 5).
+kartu(biru, 5).
 
-    kartu(merah, rev), /* REV -> REVERSE */  
-    kartu(kuning, rev),
-    kartu(hijau, rev),
-    kartu(biru, rev),
+kartu(merah, 6).
+kartu(kuning, 6).
+kartu(hijau, 6).
+kartu(biru, 6).
 
-    kartu(merah, drawtwo),
-    kartu(kuning, drawtwo),
-    kartu(hijau, drawtwo),
-    kartu(biru, drawtwo),
+kartu(merah, 7).
+kartu(kuning, 7).
+kartu(hijau, 7).
+kartu(biru, 7).
 
-    kartu(hitam, wild),
+kartu(merah, 8).
+kartu(kuning, 8).
+kartu(hijau, 8).
+kartu(biru, 8).
 
-    kartu(hitam, drawfour),
+kartu(merah, 9).
+kartu(kuning, 9).
+kartu(hijau, 9).
+kartu(biru, 9).
 
-    kartu(hitam, mimic).
+kartu(merah, skip).
+kartu(kuning, skip).
+kartu(hijau, skip).
+kartu(biru, skip).
+
+kartu(merah, rev). /* REV -> REVERSE */  
+kartu(kuning, rev).
+kartu(hijau, rev).
+kartu(biru, rev).
+
+kartu(merah, drawtwo).
+kartu(kuning, drawtwo).
+kartu(hijau, drawtwo).
+kartu(biru, drawtwo).
+
+kartu(hitam, wild).
+kartu(hitam, drawfour).
+kartu(hitam, mimic).
+
+
